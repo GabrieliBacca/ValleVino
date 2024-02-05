71d7b178cade36ff85b705e8eb8fee35904c55c1
Bruno Bride <brunexcorp@gmail.com>
2024-02-05 19:32:44 -0300

    up folder APP with VUE files

---
+++ App/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Home />
+    <Home />
+  </div>
+</template>
+
+<script>
+import "./styles/global.css"; //aplicando as configurações do css global
+
+import Header from "./components/Header.vue";
+import Home from "./pages/Home";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Home,
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ App/src/pages/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <main>
+    <!-- //? conteudo da home -->
+
+    <section class="container" id="mainContent">
+      <div class="content-container">
+        <h1 id="mainTitle">
+          Sejam muito bem-vindos a Valle Vino!<br />
+          <span class="texto-1">Vinhos de todo o MUNDO</span><br />
+          <span class="texto-2"></span>
+        </h1>
+        <p class="maintext">
+          Somos uma empresa de Blumenau, apaixonados por proporcionar momentos
+          incríveis aos nossos clientes. Nosso objetivo é fazer com que você se
+          sinta experimentando uma obra de arte engarrafada.
+        </p>
+        <div class="call-to-action-container">
+          <button><a href="#">Visite a nossa loja</a></button>
+        </div>
+      </div>
+
+      <div>
+        <div class="video-container">
+          <video autoplay muted loop id="video-bg">
+            <source src="../assets/video/home.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<!-- 
+---------------------------
+|                         |
+|         scripts         |
+|                         |
+--------------------------- 
+-->
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<!-- 
+---------------------------
+|                         |
+|           CSS           |
+|                         |
+--------------------------- 
+-->
+
+<style scoped>
+main {
+  background-color: var(--color-background);
+}
+</style>
