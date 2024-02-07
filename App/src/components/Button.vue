e2b2bfbcd53c2233e930567137ab00ba5bed572f
Bruno Bride <brunexcorp@gmail.com>
2024-02-07 17:54:04 -0300

    update style button component

--- App/src/components/Button.vue
+++ App/src/components/Button.vue
@@ -1,5 +1,6 @@
 <template>
-  <button @click="activate">teste</button>
+  <button @click="activate" id="bt">teste</button>
+
   <span v-show="active">ativado</span>
 </template>
 
@@ -27,4 +28,15 @@ export default {
 --------------------------- 
 -->
 
-<style scoped></style>
+<style scoped>
+#bt {
+  background-color: var(--color-background-dark);
+  color: var(--color-background);
+  font-size: 24px;
+  font-family: baskerville;
+  padding: 5px;
+  width: 330px;
+  height: 56px;
+  border-radius: 8px;
+}
+</style>
