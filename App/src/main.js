15778b985dfefb27e30b0273e769b22a4adea789
Bruno Bride <brunexcorp@gmail.com>
2024-02-08 16:34:25 -0300

    ...

--- App/src/main.js
+++ App/src/main.js
@@ -6,35 +6,56 @@ import "primevue/resources/themes/saga-blue/theme.css"; // Tema do PrimeVue
 import "primevue/resources/primevue.min.css"; // Estilos do PrimeVue
 import "primeicons/primeicons.css"; // Ícones do PrimeVue
 import "./styles/tailwind.css"; // Arquivo CSS gerado pelo Tailwind
+import DataView from "primevue/dataview";
+import DataViewLayoutOptions from "primevue/dataviewlayoutoptions";
 
-//componentes - reutiliza em outros componetes
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+// Importe os componentes e páginas necessários
+import Header from "./components/Header.vue";
+import WineCard from "./components/WineCard.vue";
+import PopUp from "./components/PopUp.vue";
 import Home from "./pages/Home.vue";
 import Loja from "./pages/Loja.vue";
-import Quiz from "./pages/WineQuiz.vue";
-import WineCard from "./components/WineCard.vue";
+import Loja2 from "./pages/Loja2.vue";
+>>>>>>> f33fe52a9b55fd424b4246ab78bd5db79a574b3f
 
-//paginas de login - visualização unica
-import Login from "./pages/Login/Login.vue";
-import Cadastro from "./pages/Login/Cadastro.vue";
-import EsqueceuSenha from "./pages/Login/EsqueceuSenha.vue";
-import ValidarCodigo from "./pages/Login/ValidarCodigo.vue";
-import CriarSenha from "./pages/Login/CriarSenha.vue";
-import SenhaAlterada from "./pages/Login/SenhaAlterada.vue";
+import WineQuiz from "./pages/WineQuiz.vue";
+import WineForm from "./pages/WineForm.vue";
+import WineList from "./pages/WineList.vue";
 
+// Defina as rotas
 const routes = [
-  { path: "/", component: Home }, // Rota para o componente Home
+<<<<<<< HEAD
+    { path: '/', component: Home },
+    { path: '/home', component: Home },
+    { path: '/loja', component: Loja },
+    { path: '/loja2', component: Loja2 },
+    { path: '/loja3', component: Loja3 },
+
+=======
+  { path: "/", component: Home },
+  { path: "/home", component: Home },
   { path: "/loja", component: Loja },
-  { path: "/quiz", component: Quiz },
-  { path: "/winecard", component: WineCard },
-  // Rota para o componente de login
-  { path: "/login", component: Login },
-  { path: "/cadastro", component: Cadastro },
-  { path: "/esqueceuSenha", component: EsqueceuSenha },
-  { path: "/validarCodigo", component: ValidarCodigo },
-  { path: "/criarSenha", component: CriarSenha },
-  { path: "/senhaAlterada", component: SenhaAlterada },
+  { path: "/loja2", component: Loja2 },
+>>>>>>> f33fe52a9b55fd424b4246ab78bd5db79a574b3f
+
+  { path: "/wineCard", component: WineCard },
+  { path: "/wineQuiz", component: WineQuiz },
+  { path: "/wineForm", component: WineForm },
+  { path: "/popUp", component: PopUp },
+  { path: "/wineList", component: WineList },
 ];
 
+// Crie o roteador
 const router = createRouter({
   history: createWebHistory(),
   routes,
