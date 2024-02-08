6de2ead5826e1486c12ade19126513c546cf3753
rulir7 <rulir@hotmail.com>
2024-02-08 13:47:50 -0300

    deu erro e reinstalei....atualizei o prismaa.bat com as infos do que fazer... se der erro no APP é apagar node_modules e dar npm install

--- API/prismaa.bat
+++ API/prismaa.bat
@@ -13,6 +13,14 @@ echo // npm i -D typescript @types/node @types/express @types/dotenv @types/cors
 echo // npm i --save-dev prisma esbuild-register nodemon && ^
 echo // PARA RODAR npm run dev
 echo // npx prisma studio && ^
- 
 
+echo // SE DER ERRO NO GIT.. APAGAR A PASTA NODE_MODULES e RODAR os COMANDOS
+
+echo // npm install typescript ts-node @types/node --save-dev && ^
+echo // npx tsc --init && ^
+echo // npm install prisma --save-dev && ^
+echo // npm i express dotenv cors express-validator @prisma/client && ^
+echo // npm i -D typescript @types/node @types/express @types/dotenv @types/cors && ^
+echo // npm i --save-dev prisma esbuild-register nodemon && ^
+echo // PARA RODAR npm run dev
  
