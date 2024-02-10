70c1b0256fcb40b85820ad9d21dd11d794c7ef78
rulir7 <rulir@hotmail.com>
2024-02-10 19:57:22 -0300

    arrumando o scroll

--- App/src/pages/Loja6.vue
+++ App/src/pages/Loja6.vue
@@ -73,4 +73,21 @@ export default {
 </script>
 
 
-<style scoped></style>
+<style scoped>
+.video-container {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    /* Adjust the height as needed */
+}
+
+.centered-text {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    color: white;
+    /* Add text color as needed */
+}
+</style>
