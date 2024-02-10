86eb59abfcb0ae4542828e14b8d016342a4424d6
rulir7 <rulir@hotmail.com>
2024-02-10 10:28:18 -0300

    global.css com os dados da antiga loja4 para nao perder o css

--- App/src/pages/Loja4.vue
+++ App/src/pages/Loja4.vue
@@ -8,33 +8,4 @@
 
 </script>
 
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
-
-body {
-    margin: 0;
-}
-
-button {
-    font: inherit;
-    cursor: pointer;
-    background-color: rgba(66, 185, 131, 1);
-    color: white;
-    border: 1px solid rgba(66, 185, 131, 1);
-    padding: 0.5rem 1.5rem;
-    border-radius: 30px;
-    margin-bottom: 20px;
-}
-
-button:hover,
-button:active {
-    background-color: #82dcb1;
-    border-color: #82dcb1;
-}
-</style>
+<style></style>
