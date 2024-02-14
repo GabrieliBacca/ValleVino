cb82765cdd2b13d18072127be16fb55fdba5b439
Rodrigo <devrodrigobnu@gmail.com>
2024-02-14 18:54:06 -0300

    Update about us

--- App/src/pages/AboutUs.vue
+++ App/src/pages/AboutUs.vue
@@ -1,34 +1,33 @@
 <template>
-    <div class="container ">
+    <div class="container">
         <br>
         <div class="subcontainer">
             <div class="header flex justify-start">
-
-                <h1 class="text-left">Sobre nós</h1>
+                <h1 class="text-left">Sobre nós</h1>
             </div>
             <br><br><br>
-            <div class=" text">
+            <div class="text">
                 <span>
-                    Acreditamos que o vinho é muito mais que uma bebida. Ele é uma experiência. Uma jornada que nos leva
-                    a
-                    novos
-                    horizontes sensoriais e culturais, na ValleVino, não apenas oferecemos rotulos selecionados, mas
-                    também
-                    conhecimento, harmonização e sentimentos por trás de cada garafa
+                    Acreditamos que o vinho é muito mais que uma bebida. Ele é uma experiência. Uma jornada que nos leva a
+                    novos horizontes sensoriais e culturais, na ValleVino, não apenas oferecemos rótulos selecionados, mas
+                    também conhecimento, harmonização e sentimentos por trás de cada garrafa.
                 </span>
             </div>
             <br><br><br>
             <div class="text">
-                <span class="text-left">Torcemos para que cada cliente
-                    que viva essa experiência consiga aprender e ensinar e,
-                    acima de tudo criar laços e memórias </span>
+                <span class="text-left">Torcemos para que cada cliente que viva essa experiência consiga aprender e ensinar
+                    e, acima de tudo, criar laços e memórias.</span>
             </div>
-            <br>
-            <br>
+            <br><br>
             <div class="brinde d-flex justify-end align-center">
                 <span>Vamos brindar?</span>
             </div>
             <br>
+        </div>
+        <div id="imagens-about-us">
+            <img src="../assets/img/foto1-sobre-nos.jpg" alt="">
+            <img src="../assets/img/foto2-sobre-nos.jpg" alt="">
+            <img src="../assets/img/foto3-sobre-nos.jpg" alt="">
         </div>
     </div>
 </template>
@@ -36,13 +35,10 @@
 <script>
 export default {
     setup() {
-
-
-        return {}
+        return {};
     }
 }
 </script>
-
 
 <style lang="scss" scoped>
 * {
@@ -59,8 +55,20 @@ export default {
     padding: 0;
     background: rgba(250, 247, 242, 0.25);
     box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    background-color: #f5ebda;
+}
 
+#imagens-about-us {
+    display: flex;
+    justify-content: space-around;
+}
 
+#imagens-about-us img {
+    width: calc(100% / 3 - 20px);
+    /* Definindo a largura de cada imagem com uma margem de 10px em cada lado */
+    margin: 0 10px;
+    /* Espaçamento entre as imagens */
+    box-shadow: 20px 1px 25px rgba(0, 0, 0, 0.5);
 }
 
 .header {
@@ -70,8 +78,6 @@ export default {
     font-style: italic;
     font-weight: 400;
     line-height: normal;
-
-
 }
 
 .text {
