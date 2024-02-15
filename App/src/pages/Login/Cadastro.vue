63c984275f166f1fb16f37042f657259243a41e3
Bruno <121812428+BrunoBride@users.noreply.github.com>
2024-02-15 08:49:16 -0300

    update pages view

--- App/src/pages/Login/Cadastro.vue
+++ App/src/pages/Login/Cadastro.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <form>
+    <form @submit.prevent="submit">
       <h1>
         Olá! <br />
         Registre-se para começar...
       </h1>
       <div class="textBox">
-        <input id="user" type="text" placeholder="Nome de usuário" />
-        <input id="email" type="text" placeholder="Seu email" />
+        <input v-model="name" id="user" type="text" placeholder="Nome de usuário" />
+        <input v-model="email" id="email" type="text" placeholder="Seu email" />
         <input id="password" type="password" placeholder="Senha" />
         <input
           id="confirmPassword"
@@ -17,6 +17,34 @@
       </div>
       <button type="submit" id="bt">Cadastrar</button>
     </form>
+    <div class="textWhitSocialMedia">
+      <p class="txtInfo">Ou  registre-se com</p>
+      <!-- //? bt social media -->
+      <img
+        id="loginSocialMedia"
+        src="../../assets/img/bt-Facebook.svg"
+        alt="botão login com Facebook"
+      />
+      
+      <img
+        id="loginSocialMedia"
+        src="../../assets/img/bt-Google.svg"
+        alt="botão login com Google"
+      />
+
+      <img
+        id="loginSocialMedia"
+        src="../../assets/img/bt-Apple.svg"
+        alt="botão login com Apple"
+      />
+      <p class="txtInfo">Ja possui uma conta?<a href="/"> Logar Agora!</a></p>
+    </div>
+
+    <img class=""
+        id="img-bg"
+        src="../../assets/img/inscreva-se-bg.svg"
+        alt="background Image"
+      />
   </div>
 </template>
 
@@ -62,5 +90,17 @@ h1 {
   height: 56px;
   border-radius: 8px;
   text-align: center;
+}
+#img-bg{
+width: 1440px;
+height: 710px;
+position: absolute;
+  right: 0;
+  bottom: 0;
+  min-width: 50%;
+  min-height: 50%;
+  z-index: -1;
+  filter: brightness(0.3);
+  object-fit: cover;
 }
 </style>
--- App/src/pages/Login/Login.vue
+++ App/src/pages/Login/Login.vue
@@ -10,14 +10,46 @@
       </h1>
 
       <div class="textBox">
-        <input v-model="email" type="text" placeholder="Digite seu email" />
-        <input v-model="password" type="password" placeholder="Digite sua senha" />
-        <p><a href="/esqueceusenha">Esqueceu a senha?</a></p>
+        <input v-model="email" id="email" type="text" placeholder="Digite seu email" />
+        <input
+          v-model="password" id="password"
+          type="password"
+          placeholder="Digite sua senha"
+        />
+        <p class="txtInfo"><a href="/esqueceusenha">Esqueceu a senha?</a></p>
       </div>
       <!-- //! colocar o icone de olho no text para revelar  a senha -->
 
       <button type="submit" id="bt-login">Login</button>
     </form>
+    <div class="textWhitSocialMedia">
+      <p class="txtInfo">Ou faça login com</p>
+      <!-- //? bt social media -->
+      <img
+        id="loginSocialMedia"
+        src="../../assets/img/bt-Facebook.svg"
+        alt="botão login com Facebook"
+      />
+      
+      <img
+        id="loginSocialMedia"
+        src="../../assets/img/bt-Google.svg"
+        alt="botão login com Google"
+      />
+
+      <img
+        id="loginSocialMedia"
+        src="../../assets/img/bt-Apple.svg"
+        alt="botão login com Apple"
+      />
+      <p class="txtInfo">Não tem uma conta?<a href="/"> Registre-se!</a></p>
+    </div>
+
+    <img class=""
+        id="img-bg"
+        src="../../assets/img/login-bg.svg"
+        alt="background Image"
+      />
   </div>
 </template>
 
@@ -34,11 +66,15 @@ export default {
     const password = ref("");
 
     const fetchUsuarios = () =>
-      api.get("http://localhost:8000/api/user").then((res) => (usuarios.value = res.data));
+      api
+        .get("http://localhost:8000/api/user")
+        .then((res) => (usuarios.value = res.data));
 
     const login = () => {
       // Enviar uma solicitação para o endpoint api/user para verificar as credenciais
-      const user = usuarios.value.find((user) => user.email === email.value && user.password === password.value);
+      const user = usuarios.value.find(
+        (user) => user.email === email.value && user.password === password.value
+      );
       console.log("usuarios.value:", usuarios.value);
       console.log("email.value:", email.value);
       console.log("password.value:", password.value);
@@ -61,21 +97,28 @@ export default {
 </script>
 
 <style scoped>
-/* Estilos permanecem inalterados */
-</style>
-
-<style scoped>
 body {
-  background-color: #f2f2f2;
+  background-color: var(--color-background-light);
 }
 
 h1 {
   color: var(--color-background-dark);
 }
 
+.textBox{
+  width: 388px;
+  height: 265px;
+  background-color:var(--color-background-light);
+  opacity: 0.5;
+  border: #e8ecf4 solid 1px;
+  border-radius: 8px;
+  z-index: -1;
+
+ 
+}
+
 #email,
 #password {
-  display: flex;
   background-color: #f7f8f9;
   color: #8391a1;
   font-size: 15px;
@@ -90,7 +133,7 @@ h1 {
 }
 
 #bt-login {
-  background-color: var(--color-background-dark);
+  background-color: var(--color-background-light);
   color: var(--color-background);
   font-size: 24px;
   font-family: baskerville;
@@ -99,5 +142,18 @@ h1 {
   height: 56px;
   border-radius: 8px;
   text-align: center;
+}
+
+#img-bg{
+width: 1440px;
+height: 710px;
+position: absolute;
+  right: 0;
+  bottom: 0;
+  min-width: 50%;
+  min-height: 50%;
+  z-index: -1;
+  filter: brightness(0.3);
+  object-fit: cover;
 }
 </style>
