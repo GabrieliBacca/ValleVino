cda37e82c6cefc70833467b4bbb0781e0ff705e5
Bruno <121812428+BrunoBride@users.noreply.github.com>
2024-02-16 11:22:17 -0300

    update tela senha alterada css

--- App/src/pages/Login/SenhaAlterada.vue
+++ App/src/pages/Login/SenhaAlterada.vue
@@ -1,7 +1,11 @@
 <template>
-  <div id="msg">
-    <img id="seloSucesso" src="../../assets/img/ico-sucesso.svg" />
-
+  <div class="container">
+    <img id="img-ico" src="../../assets/img/ico-sucesso.svg" alt="Selo sucesso" />
+    <img
+      class="img-bg"
+      src="../../assets/img/senha-alterada-bg.svg"
+      alt="background Image"
+    />
     <h1>Maravilha!</h1>
     <br />
     <h3>Sua senha foi alterada com sucesso.</h3>
@@ -23,11 +27,39 @@ body {
 h1 {
   color: var(--color-text-dark);
 }
-#msg {
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  margin: 45px;
-  gap: 15px;
+.container {
+  position: relative;
+  width: 100%;
+  max-width: var(--max-width);
+  margin: 0px 0px 20px 0px;
+  padding: 50px;
+  background: rgba(250, 247, 242, 0.25);
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  background-color: #f5ebda;
+  opacity: 0.9;
+}
+.img-bg {
+  width: 100vw;
+  height: 100%;
+  position: absolute;
+  object-fit: cover;
+  opacity: 0.5;
+  right: 0;
+  bottom: 0;
+  filter: brightness(0.3);
+  z-index: -1;
+}
+
+#bt-login {
+  background-color: var(--color-background-light);
+  color: var(--color-background);
+  font-size: 24px;
+  font-family: baskerville;
+  padding: 5px;
+  width: 330px;
+  height: 56px;
+  border-radius: 8px;
+  justify-content: center;
+  margin-top: 45px;
 }
 </style>
