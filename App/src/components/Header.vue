b6769a79659148f8e78006cb498e222d7cf08579
Rodrigo Joel Luchtenberg <137116564+devrodrigobnu@users.noreply.github.com>
2024-02-27 07:51:56 -0300

    ajuste no header responsividade

--- App/src/components/Header.vue
+++ App/src/components/Header.vue
@@ -1,40 +1,23 @@
 <template>
   <header :class="{ 'header-fixed': isFixedHeader }">
     <nav class="container">
-      <a href="/"><img id="logo" alt="Logo Valle Vino" src="../assets/img/logoValleVinoBlack.png" /></a>
-      <!-- //? bt menu -->
-      <img v-on:click="openMenu" id="bt-menu" src="../assets/img/ico-menu-hb.svg" alt="botão de menu lateral" />
-      <!-- //? overlay menu lateral -->
-      <div v-on:click="closeMenu" id="menu-overlay" v-if="menuActive"></div>
-
-      <!-- //? menu -->
-      <div id="menu-itens" :class="{ active: menuActive }">
-        <!-- //? logo menu -->
-
-        <img id="menu-logo" alt="Logo Valle Vino" src="../assets/img/logoValleVinoWhite.png" />
-        <!-- //? links menu -->
+      <a href="/"><img id="logo" class="logo" alt="Logo Valle Vino" src="../assets/img/logoValleVinoBlack.png" /></a>
+      <img v-on:click="openMenu" id="bt-menu" class="menu-icon" src="../assets/img/ico-menu-hb.svg"
+        alt="botão de menu lateral" />
+      <div v-on:click="closeMenu" id="menu-overlay" v-if="menuActive" class="menu-overlay"></div>
+      <div id="menu-itens" :class="{ active: menuActive }" class="menu-items">
+        <img id="menu-logo" class="logo" alt="Logo Valle Vino" src="../assets/img/logoValleVinoWhite.png" />
         <ul>
           <li><a href="/home">Home</a></li>
           <li><a href="/loja">Loja</a></li>
-          <!-- <li><a href="/slider">Slider</a></li> -->
-          <!-- <li><a href="/loja2">Loja2</a></li> -->
-          <!-- <li><a href="/loja4">Checkout</a></li> -->
-          <!-- <li><a href="/loja5">Vinhos</a></li> -->
-          <!-- <li><a href="/loja6">HOME A</a></li> -->
-          <!-- <li><a href="/wineQuiz">Quiz</a></li> -->
           <li><a href="/Adm">ADM</a></li>
-          <!-- <li><a href="/PopUp">18+</a></li> -->
           <li><a href="/WineCard">WineCard</a></li>
-          <!-- <li><a href="/WineList">WineList</a></li> -->
         </ul>
-        <!-- //? icones menu -->
-
-        <a href="/Login"><img id="bt-usuario" src="../assets/img/ico-usuario.svg" alt="botão de login usuario" /></a>
-
-        <a href="/"><img id="bt-carrinho" src="../assets/img/ico-carrinho.svg" alt="botão de carrinho" /></a>
-
-        <!-- //! colocar esse botao no rodape do menu via css -->
-        <img v-on:click="closeMenu" id="bt-esconder-menu" alt="botao esconder menu lateral"
+        <a href="/Login"><img id="bt-usuario" class="menu-icon" src="../assets/img/ico-usuario.svg"
+            alt="botão de login usuario" /></a>
+        <a href="/"><img id="bt-carrinho" class="menu-icon" src="../assets/img/ico-carrinho.svg"
+            alt="botão de carrinho" /></a>
+        <img v-on:click="closeMenu" id="bt-esconder-menu" class="menu-icon" alt="botao esconder menu lateral"
           src="../assets/img/ico-setas-direita.svg" />
       </div>
     </nav>
@@ -70,14 +53,6 @@ export default {
 };
 </script>
 
-<!-- 
----------------------------
-|                         |
-|           CSS           |
-|                         |
---------------------------- 
--->
-
 <style scoped>
 header {
   background-color: var(--color-background-light);
@@ -85,7 +60,7 @@ header {
   height: 70px;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
 }
 
 .header-fixed {
@@ -94,46 +69,25 @@ header {
   left: 10;
   width: 97.5vw;
   z-index: 10;
-  /* Make sure it's on top of other elements */
 }
 
-nav {
+.container {
   display: flex;
-  justify-content: space-between;
+  align-items: center;
 }
 
-#logo {
-  width: 130px;
-  align-content: center;
-  justify-content: center;
+.logo {
+  width: 20%;
+  max-width: 130px;
   margin: 10px;
 }
 
-#bt-usuario {
+.menu-icon {
   width: 25px;
-  display: flex;
-  flex-direction: row;
   margin: 30px;
 }
 
-#bt-carrinho {
-  width: 25px;
-  display: flex;
-  flex-direction: row;
-  margin: 30px;
-}
-
-#bt-menu {
-  width: 30px;
-  margin: 20px;
-}
-
-#bt-esconder-menu {
-  width: 25px;
-  margin-top: 50px;
-}
-
-#menu-overlay {
+.menu-overlay {
   position: fixed;
   top: 0;
   left: 0;
@@ -143,18 +97,12 @@ nav {
   opacity: 0.5;
 }
 
-#menu-logo {
-  width: 110px;
-  margin-top: 30px;
-  margin-bottom: 10px;
-}
-
-#menu-itens {
+.menu-items {
   position: fixed;
   top: 0;
   right: 0;
   background-color: var(--color-background-light);
-  width: 35%;
+  width: 80%;
   height: 100vh;
   display: none;
   flex-direction: column;
@@ -163,7 +111,7 @@ nav {
   font-size: 20px;
 }
 
-#menu-itens.active {
+.menu-items.active {
   display: flex;
 }
 
@@ -180,60 +128,22 @@ ul li a {
   color: var(--color-text-dark);
 }
 
-/* deixando responsivo para web */
-
+/* Responsive */
 @media (min-width: 700px) {
+  header {
+    height: 60px;
+  }
 
-  #menu-logo,
-  #menu-overlay,
-  #bt-esconder-menu,
-  #bt-menu {
+  .logo {
+    width: 15%;
+  }
+
+  .menu-items {
+    width: 60%;
+  }
+
+  .menu-icon {
     display: none;
   }
-
-  #menu-itens {
-    display: flex;
-    position: static;
-    height: 60px;
-    width: auto;
-    flex-direction: row;
-  }
-
-  ul {
-    display: flex;
-    height: 60px;
-    align-items: center;
-  }
-
-  ul li {
-    margin: 0;
-    margin-right: 20px;
-  }
-
-  #bt-carrinho {
-    height: 60px;
-    display: flex;
-    align-items: start;
-    justify-content: center;
-  }
-}
-
-#background-video::after {
-  content: "";
-  width: inherit;
-  height: inherit;
-  position: fixed;
-  inset: 0;
-}
-
-#video-bg {
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  min-width: 100%;
-  min-height: 100%;
-  z-index: -1;
-  filter: brightness(0.3);
-  object-fit: cover;
 }
 </style>
